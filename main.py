7897d5e62a1c39c93a317e19e15684dd63021821
Mateo Miño <118315392+Mino-Mateo@users.noreply.github.com>
2023-03-07 12:46:39 +0000

    Algoritmo Comentado

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#Funcion para Imprimir
 def imprimir_Arreglo(arreglo):
   tamanio = len(arreglo)
   for i in range(tamanio):
     print(f'[{arreglo[i]}]', end = "")
 
+#Funcion de Busqueda
 def algoritmo_Busqueda_Lineal(arreglo, sueldo):
   resultado = False
   tamanio = len(arreglo)
@@ -12,10 +14,15 @@ def algoritmo_Busqueda_Lineal(arreglo, sueldo):
       return resultado
   return resultado
 
+#Arreglo a utlizar
 listaSueldos = [20.8, 150.5, 170.5, 180.5, 190.30, 75.6, 200]
+
+#Ingreso del dato a buscar
 imprimir_Arreglo(listaSueldos)
 sueldo = float(input("\nIngrese el sueldo a encontrar: "))
 respuesta = algoritmo_Busqueda_Lineal(listaSueldos, sueldo)
+
+#Casos
 if respuesta:
   print("El sueldo fue encontrado en nuestro sistema")
 else:
